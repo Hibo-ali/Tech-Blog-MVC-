2df2805ffa0d5f1e703c19765cbe2e8791d98bb4
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-21 17:45:39 -0500

    function to delete posts and edit it

---
+++ public/edit.js
@@ -0,0 +1,34 @@
+const postId = document.querySelector("#post-id").value;
+
+const editFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title-input-post").value;
+  const body = document.querySelector("#content-input-post").value;
+
+  if (title && body) {
+    await fetch(`/api/posts/${postId}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, body }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    document.location.replace("/dashboard");
+  }
+};
+
+const deletePostHandler = async () => {
+  await fetch(`/api/posts/${postId}`, {
+    method: "DELETE",
+  });
+
+  document.location.replace("/dashboard");
+};
+
+document
+  .querySelector("#edit-form")
+  .addEventListener("submit", editFormHandler);
+
+document
+  .querySelector("#delete-btn")
+  .addEventListener("click", deletePostHandler);
