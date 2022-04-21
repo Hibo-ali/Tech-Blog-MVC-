40368e43eef511e5d09eff77695d3a376d3bb4ba
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-21 17:49:36 -0500

    new post functionality with alert for error

---
+++ public/new-post.js
@@ -0,0 +1,23 @@
+const postFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector("#title-input-post").value;
+    const body = document.querySelector("#content-input-post").value;
+  
+    if (title && body) {
+      const response = await fetch("/api/posts/", {
+        method: "POST",
+        body: JSON.stringify({ title, body }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert("Failed to post");
+      }
+    }
+  };
+  
+  document
+    .querySelector("#post-form")
+    .addEventListener("submit", postFormHandler);
